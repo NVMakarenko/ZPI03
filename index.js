cd259bea6974c646daf30b46dc981745e0864d05
NVMakarenko <taradator@ukr.net>
2021-02-27 16:23:49 +0200

    Test implimentation

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const Calc = require('./calc.js');
 console.log('Taylor series calculator welcomes you');
 var t = Calc.Taylor(1);
 console.log(t);
+
--- index.test.js
+++ index.test.js
@@ -1,6 +1,16 @@
 const assert = require('assert');
 const Calc = require('./calc.js');
 
-assert.equal(typeof Calc.Taylor, 'fuction');
-var t = Calc.Taylor(1);
+//assert.equal(typeof Calc.Taylor, 'fuction');
+//var t = Calc.Taylor(1);
 
+
+describe('Taylor series',function(){
+    t = Calc.Taylor(1);
+    it('shoud have a Taylor function defined', function (){
+        assert.equal(typeof Calc.Taylor, "function");  
+    });
+    it ('has to be equal 10', function(){
+        assert.equal(Calc.Taylor(), 10);
+       });
+})
